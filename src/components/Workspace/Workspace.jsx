3d9940e987c3a9203d190dd6ee6b721756961ef9
Dasha Yarkovska <yarkovska11@gmail.com>
2023-05-08 19:01:35 +0300

    little fix in name

---
+++ src/components/Workspace/Workspace.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./Workspace.styled";
+
+function Workspace() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Workspace;
---
+++ src/components/Workspace/Workspace.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  max-width: 1200px;
+  padding: 20px;
+  background-color: #fefefe;
+`;
